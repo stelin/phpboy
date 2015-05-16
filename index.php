f1e9d4ba7b3e01f25e9617df9be794002c19cbe2
stelin <794774870@qq.com>
2015-05-16 20:00:14 +0800

    add_file

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../../framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
-// remove the following line when in production mode
-// defined('YII_DEBUG') or define('YII_DEBUG',true);
+require_once dirname(__FILE__).'/protected/config/model.php';
+$yii=dirname(__FILE__).'/../lib/yii.php';
+$config=dirname(__FILE__).'/protected/config/'.APPLICATION_ENV.'/main.php';
 
 require_once($yii);
-Yii::createWebApplication($config)->run();
+$app = Yii::createWebApplication($config);
+$app->run();
